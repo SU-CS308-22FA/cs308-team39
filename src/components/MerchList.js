03a8b8a88580e15be8e316db998f18f8366e2ca6
bartu28 <bartudogan@sabanciuniv.edu>
2022-11-02 22:49:34 +0300

    fixed a bug

--- src/components/MerchList.js
+++ src/components/MerchList.js
@@ -1,31 +1,30 @@
-import './MerchList.css'
+import "./MerchList.css";
 import React from "react";
-import { Link } from 'react-router-dom'
-import Delete from '../delete.svg'
-import { projectFirestore } from "../firebase/config"
+import { Link } from "react-router-dom";
+import Delete from "../delete.svg";
+import { projectFirestore } from "../firebase/config";
 
 export default function MerchList({ merchs }) {
+  const handleClick = (id) => {
+    projectFirestore.collection("merchandises").doc(id).delete();
+  };
 
-    const handleClick =  (id) => {
-         projectFirestore.collection('merchandises').doc(id).delete()
-    }
-
-    return (
-        <div className="merch-list">
-            {merchs.map(merch => (
-                <div key={merch.id} className="card">
-                    <h3>{merch.title}</h3>
-                    <div>{merch.description.substring(0,50)}...</div>
-                    <p>{merch.rating} out of 5</p>
-                    <p>{merch.price} TL</p>
-                    <Link to={`/merch/${merch.id}`}>More</Link>
-                    <img
-                        className='delete'
-                        src={Delete}
-                        onClick={() => handleClick(merch.id)}
-                    />
-                </div>        
-            ))}
+  return (
+    <div className="merch-list">
+      {merchs.map((merch) => (
+        <div key={merch.id} className="card">
+          <h3>{merch.title}</h3>
+          <div>{merch.description.substring(0, 50)}...</div>
+          <p>{merch.rating} out of 5</p>
+          <p>{merch.price} TL</p>
+          <Link to={`/merch/${merch.id}`}>More</Link>
+          <img
+            className="delete"
+            src={Delete}
+            onClick={() => handleClick(merch.id)}
+          />
         </div>
-    )
+      ))}
+    </div>
+  );
 }

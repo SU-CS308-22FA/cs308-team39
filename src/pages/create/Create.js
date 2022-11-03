e346315abdaedd3610e86ec2df18e0840e2b6589
bartu28 <bartudogan@sabanciuniv.edu>
2022-11-03 13:19:55 +0300

    rating changed to quantity

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,76 +1,73 @@
-import './Create.css'
+import "./Create.css";
 import React from "react";
-import { useState } from 'react'
-import { useHistory } from 'react-router-dom';
-import { projectFirestore } from '../../firebase/config'
-
-
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
+import { projectFirestore } from "../../firebase/config";
 
 export default function Create() {
-    const [title, setTitle] = useState('')
-    const [description, setDescription] = useState('')
-    const [rating, setRating] = useState('')
-    const [price, setPrice] = useState('')
-    const history = useHistory()
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [rating, setRating] = useState("");
+  const [price, setPrice] = useState("");
+  const history = useHistory();
 
-    const handleSubmit = async (e) => {
-        e.preventDefault()
-        const doc = {title, description, price, rating}
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const doc = { title, description, price, rating };
 
-        try {
-          await projectFirestore.collection('merchandises').add(doc)
-          history.push('/')
-        } catch(err) {
-          console.log(err)
-        }
-      }
-    
-      return (
-        <div className="create">
-          <h2 className="page-title">Add a New Merchandise</h2>
-          <form onSubmit={handleSubmit}>
-    
-            <label>
-              <span>Merchandise title:</span>
-              <input 
-                type="text" 
-                onChange={(e) => setTitle(e.target.value)}
-                value={title}
-                required
-              />
-            </label>
-    
-            <label>
-              <span>Merchandise Description:</span>
-              <textarea 
-                onChange={(e) => setDescription(e.target.value)}
-                value={description}
-                required
-              />
-            </label>
-    
-            <label>
-              <span>Price (TL):</span>
-              <input 
-                type="number" 
-                onChange={(e) => setPrice(e.target.value)}
-                value={price}
-                required 
-              />
-            </label>
+    try {
+      await projectFirestore.collection("merchandises").add(doc);
+      history.push("/");
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
-            <label>
-              <span>Rating (out of 5):</span>
-              <input 
-                type="number" 
-                onChange={(e) => setRating(e.target.value)}
-                value={rating}
-                required 
-              />
-            </label>
-    
-            <button className="btn">submit</button>
-          </form>
-        </div>
-      )
+  return (
+    <div className="create">
+      <h2 className="page-title">Add a New Merchandise</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>Merchandise title:</span>
+          <input
+            type="text"
+            onChange={(e) => setTitle(e.target.value)}
+            value={title}
+            required
+          />
+        </label>
+
+        <label>
+          <span>Merchandise Description:</span>
+          <textarea
+            onChange={(e) => setDescription(e.target.value)}
+            value={description}
+            required
+          />
+        </label>
+
+        <label>
+          <span>Price (TL):</span>
+          <input
+            type="number"
+            onChange={(e) => setPrice(e.target.value)}
+            value={price}
+            required
+          />
+        </label>
+
+        <label>
+          <span>Quantitiy:</span>
+          <input
+            type="number"
+            onChange={(e) => setRating(e.target.value)}
+            value={rating}
+            required
+          />
+        </label>
+
+        <button className="btn">submit</button>
+      </form>
+    </div>
+  );
 }
--- src/pages/merch/Merch.js
+++ src/pages/merch/Merch.js
@@ -1,53 +1,54 @@
 import React from "react";
-import './Merch.css'
-import { useParams } from 'react-router-dom'
+import "./Merch.css";
+import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
-import { projectFirestore } from '../../firebase/config'
-
+import { projectFirestore } from "../../firebase/config";
 
 export default function Merch() {
-    const { id } = useParams()
+  const { id } = useParams();
 
-    const [merch, setMerch] = useState(null)
-    const [isPending, setIsPending] = useState(false)
-    const [error, setError] = useState(null)
-    
-    useEffect(() => {
-        setIsPending(true)
+  const [merch, setMerch] = useState(null);
+  const [isPending, setIsPending] = useState(false);
+  const [error, setError] = useState(null);
 
-        const unsub = projectFirestore.collection('merchandises').doc(id).onSnapshot((doc) => {
-            if (doc.exists) {
-                setIsPending(false)
-                setMerch(doc.data())
-            } else {
-                setIsPending(false)
-                setError('Could not find the merchandise')
-            }
-        })
+  useEffect(() => {
+    setIsPending(true);
 
-        return () => unsub
+    const unsub = projectFirestore
+      .collection("merchandises")
+      .doc(id)
+      .onSnapshot((doc) => {
+        if (doc.exists) {
+          setIsPending(false);
+          setMerch(doc.data());
+        } else {
+          setIsPending(false);
+          setError("Could not find the merchandise");
+        }
+      });
 
-    }, [id])
+    return () => unsub;
+  }, [id]);
 
-    const handleClick = () => {
-        projectFirestore.collection('merchandises').doc(id).update({
-            title: 'Title updated'
-        })
-    }
+  const handleClick = () => {
+    projectFirestore.collection("merchandises").doc(id).update({
+      title: "Title updated",
+    });
+  };
 
-    return (
-        <div className="merch">
-            {error && <p className="error">{error}</p>}
-            {isPending && <p className="loading">Loading...</p>}
-            {merch && (
-                <>
-                   <h2 className="page-title">{merch.title}</h2> 
-                   <p>{merch.description}</p>
-                    <p>{merch.rating} out of 5</p>
-                    <p>{merch.price} TL</p>
-                    <button onClick={handleClick}>Update</button>
-                </>
-            )}
-        </div>
-    )
+  return (
+    <div className="merch">
+      {error && <p className="error">{error}</p>}
+      {isPending && <p className="loading">Loading...</p>}
+      {merch && (
+        <>
+          <h2 className="page-title">{merch.title}</h2>
+          <p>{merch.description}</p>
+          <p>Quantity {merch.rating}</p>
+          <p>{merch.price} TL</p>
+          <button onClick={handleClick}>Update</button>
+        </>
+      )}
+    </div>
+  );
 }

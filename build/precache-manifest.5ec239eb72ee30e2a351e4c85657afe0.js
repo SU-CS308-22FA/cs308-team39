6d73669e54ae80d0167d8b8656eedd6c2d1d550f
bartu28 <bartudogan@sabanciuniv.edu>
2022-11-17 15:18:54 +0300

    optimized build

---
+++ build/precache-manifest.5ec239eb72ee30e2a351e4c85657afe0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "77cb7993c7bf21c7c3b6",
+    "url": "/static/css/main.9a843a71.chunk.css"
+  },
+  {
+    "revision": "77cb7993c7bf21c7c3b6",
+    "url": "/static/js/main.f2ad4e7b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "68ea9e6df86301311793",
+    "url": "/static/js/2.185e37ed.chunk.js"
+  },
+  {
+    "revision": "37c8a2749683758a01d3c319e849c82a",
+    "url": "/static/media/delete.37c8a274.svg"
+  },
+  {
+    "revision": "9ebd6ef5bc10644f1c37599b6578fdd6",
+    "url": "/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.648dbb24af408a9e0c9764ac91af5eb6.js"
+  "/precache-manifest.5ec239eb72ee30e2a351e4c85657afe0.js"
 );
 
 workbox.clientsClaim();

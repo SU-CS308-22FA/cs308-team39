cc52463328e1422bd0da971a5cc39aeffc0dd28b
LiderB <71089442+LiderB@users.noreply.github.com>
2022-11-22 20:59:39 +0300

    edited the searchbar functionality

--- src/pages/Search/Search.js
+++ src/pages/Search/Search.js
@@ -1,21 +1,28 @@
 import React from "react";
 import MerchList from "../../components/MerchList";
 import { useEffect, useState } from "react";
-
+import { useLocation } from "react-router-dom";
 import { projectFirestore } from "../../firebase/config";
 
 // styles
 import './Search.css'
 
 
+
 export default function Search() {
   const [data, setData] = useState(null);
   const [isPending, setIsPending] = useState(false);
   const [error, setError] = useState(null);
+  const [query, setQuery] = useState("Besiktas");
+  const queryString = useLocation()
+  
+  const queryParams = new URLSearchParams(queryString)
+  
 
   useEffect(() => {
     setIsPending(true);
-
+    setQuery(queryParams.get('q'))
+    console.log("query is " + query)
     const unsub = projectFirestore.collection("merchandises").onSnapshot(
       (snapshot) => {
         if (snapshot.empty) {
@@ -24,7 +31,8 @@ export default function Search() {
         } else {
           let results = [];
           snapshot.docs.forEach((doc) => {
-            // console.log(doc)
+            console.log(doc.data().title.includes(query))
+            if(doc.data)
             results.push({ ...doc.data(), id: doc.id });
           });
           setData(results);

88be7d186b0d766a460db798a4f589697719a2ee
bartu28 <bartudogan@sabanciuniv.edu>
2022-11-27 04:52:36 +0300

    added profile pic and typeChange

--- src/pages/UserPage/UserPage.js
+++ src/pages/UserPage/UserPage.js
@@ -16,19 +16,23 @@ export default function UserPage() {
     updateUserType,
   } = useUpdateUser();
 
-  const [flag, setFlag] = useState(0);
+  const [flag, setFlag] = useState(-1);
   const [newUsername, setNewUsername] = useState(user.displayName);
   const [newEmail, setNewEmail] = useState(user.email);
   const [newPassword, setNewPassword] = useState(user.password);
+  const [newType, setNewType] = useState(null);
+  const [newPic, setNewPic] = useState(null);
+  const [key, setKey] = useState("");
 
   const [error, setError] = useState(null);
   const [type, setType] = useState(null);
+  const [pic, setPic] = useState(null);
   const [password, setPassword] = useState(null);
   useEffect(() => {
     setError(null);
     try {
       console.log("loading profile page");
-      console.log("user id:", user.uid);
+      console.log("uid: ", user.uid);
       const fetchData = async () => {
         const userRef = projectFirestore.collection("users").doc(user.uid);
         const doc = await userRef.get();
@@ -38,6 +42,7 @@ export default function UserPage() {
           console.log("Document data:", doc.data());
         }
         //console.log("doc: ", doc);
+        setPic(doc.get("pic"));
         setType(doc.get("type"));
         console.log("type:", doc.get("type"));
         setPassword(doc.get("password"));
@@ -81,15 +86,35 @@ export default function UserPage() {
     const doc = await userRef.get();
     console.log("Document data:", doc.data());
   };
-
+  const updateTypeHandleSubmit = async (e) => {
+    //try to update account name
+    e.preventDefault();
+    if ((await updateUserType(newType, key)) && !updateError) {
+      window.location.href = "/UserPage/" + newUsername;
+    }
+    const userRef = projectFirestore.collection("users").doc(user.uid);
+    const doc = await userRef.get();
+    console.log("Document data:", doc.data());
+  };
   return (
     <>
       {user && !error && (
         <div id="wrapper">
           <div id="first">
             <p>My Profile</p>
+            <img width="100" height="100" src={pic} alt="ProfilePicture"></img>
           </div>
           <div id="second">
+            {flag === -1 && (
+              <button className="btnUserPage" onClick={() => setFlag(0)}>
+                Update
+              </button>
+            )}
+            {flag === 0 && (
+              <button className="btnUserPage" onClick={() => setFlag(-1)}>
+                Cancel
+              </button>
+            )}
             <h4>
               Name: {user.displayName}
               {flag === 0 && (
@@ -187,7 +212,44 @@ export default function UserPage() {
                 </>
               )}
             </h4>
-            <h4>Account type: {type}</h4>
+            <h4>
+              Account type: {type}{" "}
+              {flag === 0 && (
+                <button className="btnUserPage" onClick={() => setFlag(4)}>
+                  Update
+                </button>
+              )}
+              {flag === 4 && (
+                <>
+                  <form onSubmit={updateTypeHandleSubmit}>
+                    <label>
+                      <p>New Type:</p>
+                      <input
+                        type="text"
+                        onChange={(e) => setNewType(e.target.value)}
+                        value={newType}
+                      />
+                      <p>Key:</p>
+                      <input
+                        type="text"
+                        onChange={(e) => setKey(e.target.value)}
+                        value={key}
+                      />
+                    </label>
+                    {!updatePending && (
+                      <button className="btn">Set Type</button>
+                    )}
+                    {updateError && <p>{updateError}</p>}
+                    {updatePending && (
+                      <button className="btn">isPending</button>
+                    )}
+                  </form>
+                  <button className="btn" onClick={() => setFlag(0)}>
+                    Cancel
+                  </button>
+                </>
+              )}
+            </h4>
           </div>
         </div>
       )}

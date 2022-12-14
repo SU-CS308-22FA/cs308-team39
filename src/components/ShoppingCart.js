001849aa4706ed48f347452e94c228d49b966ea8
LiderB <71089442+LiderB@users.noreply.github.com>
2022-12-15 01:59:06 +0300

    Delete ShoppingCartItems, add item info to cart
    Delete the unnecessary ShoppingCartItems component. Add information
    about the merchandise that are added to cart to database.

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -1,38 +1,32 @@
 import React, { useState, useEffect} from "react";
 import "./ShoppingCart.css";
 import { AiFillCloseCircle } from "react-icons/ai";
+import firebase from "firebase";
 
 import { useAuthContext } from "../hooks/useAuthContext";
 import { projectFirestore } from "../firebase/config";
-import ShoppingCartItems from "./ShoppingCartItems";
+import { RiDeleteBin6Line } from "react-icons/ri";
 
 export default function ShoppingCart() {
-    const [pending, setPending] = useState(false)
     const [products, setProducts] = useState([])
     const { user } = useAuthContext();
-    
-    console.log("1")
+   
 
     useEffect(() => {
         const fnction = async () => {
-            console.log("2")
-            setPending(true)
-            const cart = await projectFirestore.collection("carts").doc(user.uid).get()
             
-            cart.data().merchIds.forEach(async element => {
-               const a = await projectFirestore.collection("merchandises").doc(element).get()
-               products.push(a.data())
-            });
-            setPending(false)
-            console.log("3")
+            const cart = await projectFirestore.collection("carts").doc(user.uid).get()
+            const a = await projectFirestore.collection("merchandises").where(firebase.firestore.FieldPath.documentId(), 'in', cart.data().merchIds).get()
+            console.log(a.docs[1].data().title)
+            setProducts(a.docs)
             
         };
         
         fnction()
-        setProducts(products)
+        
     }, [])
 
-    console.log("4")
+    
     
 
 	return (
@@ -49,7 +43,78 @@ export default function ShoppingCart() {
 						/>
 					</button>
 				</div>
-				{!pending && <ShoppingCartItems products = {products}/>}
+				{products && <div className="cart-products">
+					{products.length === 0 && (
+						<span className="empty-text">
+							Your basket is
+							currently empty
+						</span>
+					)}
+					{products.map((product) => (
+						<div
+							className="cart-product"
+							>
+							<img
+								src={
+									product.data().imageURL
+								}
+								alt={product.data().title}
+							/>
+							<div className="product-info">
+								<h3>
+									{product.data().title}
+								</h3>
+								<span className="product-price">
+									{product.data().price}
+									TL
+								</span>
+							</div>
+							<select
+								className="count"
+								value={
+									product.count
+								}
+								>
+								{[
+									...Array(
+										10
+									).keys(),
+								].map(
+									(number) => {
+										const num =
+											number +
+											1;
+										return (
+											<option
+												value={
+													num
+												}
+												key={
+													num
+												}>
+												{
+													num
+												}
+											</option>
+										);
+									}
+								)}
+							</select>
+							<button
+								className="btn remove-btn"
+								>
+								<RiDeleteBin6Line
+									size={20}
+								/>
+							</button>
+						</div>
+					))}
+					{products.length > 0 && (
+						<button className="btn checkout-btn">
+							Proceed to checkout
+						</button>
+					)}
+				</div>}
 			</div>
 		</div>
 	);

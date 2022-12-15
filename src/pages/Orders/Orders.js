56723784bc8e7fe1539724c139014d815d4b6188
LiderB <71089442+LiderB@users.noreply.github.com>
2022-12-15 05:52:00 +0300

    Display all of the orders of a team
    Show what orders a team got in the Orders Component. Display the image,
    price, and the title of the products that are ordered.

--- src/pages/Orders/Orders.js
+++ src/pages/Orders/Orders.js
@@ -4,10 +4,11 @@ import "./Orders.css";
 import { useAuthContext } from "../../hooks/useAuthContext"; 
 import { useEffect, useState } from "react";
 import { projectFirestore } from "../../firebase/config";
+import firebase from "firebase";
 
 export default function Orders() {
     const { user } = useAuthContext();
-    const [orders, setOrders] = useState([{displayName: String},]);
+    const [orders, setOrders] = useState([]);
     let tempOrders = []
     
     useEffect(() => {
@@ -22,20 +23,20 @@ export default function Orders() {
                 const a = await projectFirestore.collection("orders").where("team", "==", doc.data().type).get()
                 console.log(a)
                 console.log(a.docs[0].data())
-                /*const merchIds = []
+                const merchIds = []
                 a.docs.forEach(element => {
                      merchIds.push(element.data().merchId)
-                });*/
+                });
 
-
-
-                a.docs.forEach(async element => {
+                const b = await projectFirestore.collection("merchandises").where(firebase.firestore.FieldPath.documentId(), 'in', merchIds).get()
+                setOrders(b.docs)
+                /*a.docs.forEach(async element => {
                     const merch = await projectFirestore.collection("merchandises").doc(element.data().merchId).get()
                     const order = {displayName: element.data().displayName, ...merch.data()}
                     console.log(order)
                     tempOrders.push(order)
                 });
-                setOrders(tempOrders)
+                setOrders(tempOrders)*/
             } 
           });
     
@@ -46,11 +47,28 @@ export default function Orders() {
     return(
         <div>
             <div>Orders For {user.displayName}</div>
-            {tempOrders.map((order) => (
+            {orders.map((order) => (
                 <div>
-                    <p>{order.displayName}</p>
-                    <p>{order.title}</p>
-                    <p>{order.imageURL}</p>
+                    <div
+							className="cart-product"
+							>
+							<img className="image"
+								src={
+									order.data().imageURL
+								}
+								alt={order.data().title}
+							/>
+							<div className="product-info">
+								<h3>
+									{order.data().title}
+								</h3>
+								<span className="product-price">
+									{order.data().price}
+									TL
+								</span>
+							</div>
+							
+						</div>
                 </div>
             ))}
         </div>

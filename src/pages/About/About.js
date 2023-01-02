781cb297f69c5b78a77fc6d03202ac7e6da2362d
Shah <shahmyrshah10@gmail.com>
2023-01-02 19:59:07 +0300

    About Page Update

---
+++ src/pages/About/About.js
@@ -0,0 +1,16 @@
+import React from "react";
+import styles from "./About.css";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>We Are The Turkish Football Federation</h1>
+      <h3>
+        The Turkish Football Federation is the governing body of association
+        football in Turkey. It was formed on 23 April 1923, and joined FIFA the
+        same year and UEFA in 1962. It organizes the Turkey national football
+        team, the Turkish Football League and the Turkish Cup
+      </h3>
+    </div>
+  );
+}

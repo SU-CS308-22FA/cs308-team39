baf291d71332ab7730b308a1f31249b24c5d4308
Shah <shahmyrshah10@gmail.com>
2023-01-02 23:27:24 +0300

    Global Styles Fix

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -9,21 +9,25 @@ export default function Contact() {
         alt="TFF Building"
         src={process.env.PUBLIC_URL + "/TFF.jpg"}
       ></img>
-      <h1>Dear Customer, You Can Contact Us Through The Following Means</h1>
+      <h1 className="DearCustomer">
+        Dear Customer, You Can Contact Us Through The Following Means
+      </h1>
       <div className="Info">
-        <h3>Local:</h3>
-        <li>Telephone : + 0090 0 216 554 51 00 + 0090 0 216 554 51 01 </li>
-        <li> Fax: + 0090 216 319 19 45 </li>
-        <li>E-Mail: iletisim@tff.org</li>
-        <li>
+        <h3 className="ContactHeader">Local:</h3>
+        <li className="ContactList">
+          Telephone : + 0090 0 216 554 51 00 + 0090 0 216 554 51 01{" "}
+        </li>
+        <li className="ContactList"> Fax: + 0090 216 319 19 45 </li>
+        <li className="ContactList">E-Mail: iletisim@tff.org</li>
+        <li className="ContactList">
           Address :Hasan Doğan Milli Takımlar Kamp ve Eğitim Tesisleri
           Riva/Beykoz/İstanbul
         </li>
-        <h3>International:</h3>
-        <li>Telephone : + 0090 216 554 57 30</li>
-        <li> Fax: + 0090 216 319 19 45 </li>
-        <li>E-Mail: intdept@tff.org</li>
-        <li>
+        <h3 className="ContactHeader">International:</h3>
+        <li className="ContactList">Telephone : + 0090 216 554 57 30</li>
+        <li className="ContactList"> Fax: + 0090 216 319 19 45 </li>
+        <li className="ContactList">E-Mail: intdept@tff.org</li>
+        <li className="ContactList">
           Address : HHasan Doğan Milli Takımlar Kamp ve Eğitim Tesisleri
           Riva/Beykoz/İstanbul
         </li>

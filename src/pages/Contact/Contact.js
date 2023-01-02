ed070d32d46cc0e96545ca3a1d79dd950b8d65e3
Shah <shahmyrshah10@gmail.com>
2023-01-02 21:27:12 +0300

    Contact Page Finished

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -4,21 +4,30 @@ import "./Contact.css";
 export default function Contact() {
   return (
     <div className="Body">
-      <h1>Dear Customer, You Can Contact Us Through The Following Means</h1>
       <img
-        className="President"
-        alt="TFF President"
-        src={process.env.PUBLIC_URL + "/President.jpg"}
+        className="TFF"
+        alt="TFF Building"
+        src={process.env.PUBLIC_URL + "/TFF.jpg"}
       ></img>
-      <p>
-        Mehmet Büyükekşi, the new President of the Turkish Football Federation
-      </p>
-      <h3>
-        The Turkish Football Federation is the governing body of association
-        football in Turkey. It was formed on 23 April 1923, and joined FIFA the
-        same year and UEFA in 1962. It organizes the Turkey national football
-        team, the Turkish Football League and the Turkish Cup
-      </h3>
+      <h1>Dear Customer, You Can Contact Us Through The Following Means</h1>
+      <div className="Info">
+        <h3>Local:</h3>
+        <li>Telephone : + 0090 0 216 554 51 00 + 0090 0 216 554 51 01 </li>
+        <li> Fax: + 0090 216 319 19 45 </li>
+        <li>E-Mail: iletisim@tff.org</li>
+        <li>
+          Address :Hasan Doğan Milli Takımlar Kamp ve Eğitim Tesisleri
+          Riva/Beykoz/İstanbul
+        </li>
+        <h3>International:</h3>
+        <li>Telephone : + 0090 216 554 57 30</li>
+        <li> Fax: + 0090 216 319 19 45 </li>
+        <li>E-Mail: intdept@tff.org</li>
+        <li>
+          Address : HHasan Doğan Milli Takımlar Kamp ve Eğitim Tesisleri
+          Riva/Beykoz/İstanbul
+        </li>
+      </div>
       <a href="https://goo.gl/maps/CzBqTgjCMLassxSK7">
         <img className="Map" src={process.env.PUBLIC_URL + "/Map.jpg"}></img>
       </a>

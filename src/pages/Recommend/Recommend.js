3e2258f016ea2cb13df462bc615c3a32dad1160b
Shah <shahmyrshah10@gmail.com>
2023-01-04 19:45:10 +0300

    Recommend Final

--- src/pages/Recommend/Recommend.js
+++ src/pages/Recommend/Recommend.js
@@ -13,6 +13,11 @@ export default function Recommend() {
   useEffect(() => {
     setIsPending(true);
 
+    function randomIntFromInterval(min, max) {
+      // min and max included
+      return Math.floor(Math.random() * (max - min + 1) + min);
+    }
+
     const unsub = projectFirestore.collection("merchandises").onSnapshot(
       (snapshot) => {
         if (snapshot.empty) {
@@ -20,16 +25,25 @@ export default function Recommend() {
           setIsPending(false);
         } else {
           let results = [];
-          let query = "jacket";
+          const categories = ["shirt", "jacket", "shoes", "flag", "football"];
+          const teams = [
+            "karsiyaka",
+            "besiktas",
+            "galatsaray",
+            "fenerbahce",
+            "trabzonspor",
+            "umraniyespor",
+            "konyaspor",
+          ];
+          const rndCat = randomIntFromInterval(0, 4);
+          let query = categories[rndCat];
+          const rndTeam = randomIntFromInterval(0, 6);
+          let query2 = teams[rndTeam];
           snapshot.docs.forEach((doc) => {
             // console.log(doc)
-            if (
-              doc
-                .data()
-                .category.toLowerCase()
-                .trim()
-                .includes(query.toLowerCase().trim())
-            )
+            if (doc.data().category.toLowerCase().trim().includes(query))
+              results.push({ ...doc.data(), id: doc.id });
+            else if (doc.data().team.toLowerCase().trim().includes(query2))
               results.push({ ...doc.data(), id: doc.id });
           });
           setData(results);
